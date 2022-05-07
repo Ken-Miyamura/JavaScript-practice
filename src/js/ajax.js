27b2188856810da60270143b8bdbc73448487f7d
“Your <mykmiyamu@gmail.com>
2022-05-07 16:30:43 +0900

    eighth commit in the middle of ajax

--- src/js/ajax.js
+++ src/js/ajax.js
@@ -1,20 +1,52 @@
 /**
  * GitHubのユーザー情報を取得するAPIを叩く
  */
-
-// GitHubのユーザーDヲ所得し、getでHTTPリクエストを送信する関数
 function fetchUserInfo(userId) {
-    fetch(`https://api.github.com/users/${encodeURIComponent(userId)}`)
-    .then(res => {
+    fetch(
+        `https://api.github.com/users/${encodeURIComponent(userId)}`
+    ).then(res => {
         console.log(res.status);
-        if (!res.ok) {
-            console.error("エラーレスポンス", response);
+        if(!res.ok) {
+            console.log("エラーレスポンス", res);
         } else {
-            return res.json().then(userInfo => {
-                console.log(userInfo);      
+            return res.json()
+            .then(userInfo => {
+                // HTMLの組み立て
+                const view = escapeHtml`
+                    <h4>${userInfo.name} (@${userInfo.login})</h4>
+                    <img src="${userInfo.avatar_url}" alt="${userInfo.login}" height="100">
+                    <dl>
+                        <dt>Location</dt>
+                        <dd>${userInfo.location}</dd>
+                        <dt>Repositories</dt>
+                        <dd>${userInfo.public_repos}</dd>
+                    </dl>
+                `;
+                // HTMLの挿入
+                const result = document.getElementById('result');
+                result.innerHTML = view;
             });
         }
-    }).catch(err => {1
-        console.error(err);
+    }).catch(err => {
+        console.log(err);
     });
+}
+
+// 特殊な記号に対するエスケープ処理
+function escapeSpecialChars(str) {
+    return str.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;").replace(/"/g, "&quot;").replace(/'/g, "&#039;");
+}
+
+// 文字列リテラルと値が元の順番どおりに並ぶように文字列を組み立てつつ、 値が文字列型であればエスケープするタグ関数
+function escapeHtml(strings, ...values) {
+    // console.log(values);
+    return strings.reduce((result, str, i) => {
+        // console.log(result);
+        const value = values[i - 1];
+        if(typeof value === 'string') {
+            return result + escapeSpecialChars(value) + str;
+        } else {
+            return result + String(value) + str;
+        }
+    })
 }
