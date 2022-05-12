e07abd54506e817b4ea888365d7f45130f015e9f
“Your <mykmiyamu@gmail.com>
2022-05-12 09:32:38 +0900

    tenth commit callback

--- src/js/asynchronous.js
+++ src/js/asynchronous.js
@@ -15,4 +15,51 @@ const interValid = setInterval(() => {
     console.log(`${i}: ${now}`);
 }, 5*1000);
 
-// スコープと非同期の実行
+
+/** スコープと非同期の実行
+ * コールバックはスコープ（クロージャ）の全てにアクセスできることに注意
+ */
+function countDown() {
+    // let i;
+    console.log('カウントダウン：');
+    for(let i=5; i>=0; i--) {
+        setTimeout(function() {
+            console.log(i === 0 ? 'GO!!' : i);
+        }, (5 - i)*1000)
+    }
+}
+
+countDown();
+
+/** エラーファーストコールバック
+ * 1000ミリ秒未満のランダムなタイミングでレスポンスを疑似的にデータ取得する関数
+ * 指定した`path`にデータがある場合は`callback(null, レスポンス)`を呼ぶ
+ * 指定した`path`にデータがない場合は`callback(エラー)`を呼ぶ
+ */
+function dummyFetch(path, callback) {
+    setTimeout(() => {
+        if(path.startsWith("/js")) {
+            callback(null, {body: `response body of ${path}`});
+        } else {
+            callback(new Error("Not Found"))
+        }
+    }, 1000 * Math.random());
+}
+
+dummyFetch("/js/ajax.js", (err, res) => {
+    if(err) {
+        console.error(err);
+    } else {
+        console.log(res);
+    }
+})
+
+dummyFetch("/ts/index.ts", (err, res) => {
+    if(err) {
+        console.error(err.message);
+    } else {
+        console.log(res);
+    }
+})
+
+// Promise
--- src/js/server.js
+++ src/js/server.js
@@ -6,51 +6,83 @@ const server = http.createServer((request, response) => {
     console.log(url);
     if ('/' === url) {
         fs.readFile('src/index.html','utf-8', (err, data) => {
-            response.writeHead(200, { 'Content-Type': 'text/html' });
-            response.write(data);
-            response.end();
+            if(err) {
+                console.log(err.message);
+            } else {
+                response.writeHead(200, { 'Content-Type': 'text/html' });
+                response.write(data);
+                response.end();
+            }
         });
     } else if ('/css/main.css' === url) {
         fs.readFile('src/css/main.css', 'utf-8', (err, data) => {
-            response.writeHead(200, { 'Content-Type': 'text/css'});
-            response.write(data);
-            response.end();
+            if(err) {
+                console.log(err.message);
+            } else {
+                response.writeHead(200, { 'Content-Type': 'text/css'});
+                response.write(data);
+                response.end();
+            }
         })
     } else if ('/js/index.js' === url) {
         fs.readFile('src/js/index.js', 'utf-8', (err, data) => {
-            response.writeHead(200, { 'Content-Type': 'text/javascript'});
-            response.write(data);
-            response.end();
+            if(err) {
+                console.log(err.message);
+            } else {
+                response.writeHead(200, { 'Content-Type': 'text/javascript'});
+                response.write(data);
+                response.end();
+            }
         });
     } else if ('/js/obj.js' === url) {
         fs.readFile('src/js/obj.js', 'utf-8', (err, data) => {
-            response.writeHead(200, { 'Content-Type': 'text/javascript'});
-            response.write(data);
-            response.end();
+            if(err) {
+                console.log(err.message);
+            } else {
+                response.writeHead(200, { 'Content-Type': 'text/javascript'});
+                response.write(data);
+                response.end();
+            }
         });
     } else if ('/js/map_set.js' === url) {
         fs.readFile('src/js/map_set.js', 'utf-8', (err, data) => {
-            response.writeHead(200, { 'Content-Type': 'text/javascript'});
-            response.write(data);
-            response.end();
+            if(err) {
+                console.log(err.message);
+            } else {
+                response.writeHead(200, { 'Content-Type': 'text/javascript'});
+                response.write(data);
+                response.end();
+            }
         });
     } else if ('/js/iterator_generator.js' === url) {
         fs.readFile('src/js/iterator_generator.js', 'utf-8', (err, data) => {
-            response.writeHead(200, { 'Content-Type': 'text/javascript'});
-            response.write(data);
-            response.end();
+            if(err) {
+                console.log(err.message);
+            } else {
+                response.writeHead(200, { 'Content-Type': 'text/javascript'});
+                response.write(data);
+                response.end();
+            }
         });
     } else if ('/js/ajax.js' === url) {
         fs.readFile('src/js/ajax.js', 'utf-8', (err, data) => {
-            response.writeHead(200, { 'Content-Type': 'text/javascript'});
-            response.write(data);
-            response.end();
+            if(err) {
+                console.log(err.message);
+            } else {
+                response.writeHead(200, { 'Content-Type': 'text/javascript'});
+                response.write(data);
+                response.end();
+            }
         });
     } else if ('/js/asynchronous.js' === url) {
         fs.readFile('src/js/asynchronous.js', 'utf-8', (err, data) => {
-            response.writeHead(200, { 'Content-Type': 'text/javascript'});
-            response.write(data);
-            response.end();
+            if(err) {
+                console.log(err.message);
+            } else {
+                response.writeHead(200, { 'Content-Type': 'text/javascript'});
+                response.write(data);
+                response.end();
+            }
         });
     }
 });
