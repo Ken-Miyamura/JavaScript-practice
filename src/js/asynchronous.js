826f76e596320e65f69bdccb02f9c99f64f921d3
“Your <mykmiyamu@gmail.com>
2022-05-18 17:06:52 +0900

    twelfth commit promise.all & promise.race

--- src/js/asynchronous.js
+++ src/js/asynchronous.js
@@ -123,7 +123,8 @@ function countDown(seconds) {
                     return reject(new Error("この数は不吉である"))
                 } 
                 if (i > 0) console.log(i + '...');
-                else resolve(console.log('GO!!'))
+                // returnの代わりに正常終了したことを表すresolveを返す
+                else resolve(console.log('GO!!'));
             }, (seconds - i)*1000))
         }
     });
@@ -147,3 +148,87 @@ countDown(10)
 .catch(function(err) {
     console.error(`管制塔、トラブル発生....${err.message}`);
 })
+
+/** 
+ * Promise.all
+ * timeoutMs秒後にresolveする
+ * Promiseインスタンスの配列を受け取り、新しいpromiseインスタンスを返す
+ * 全てのインスタンスがonFilledだった場合は、返り値のPromiseインスタンスもFulfilledとなる
+ * 一つでもrejectになったら、返り値のPromiseインスタンスもRejectedとなる
+ */ 
+
+function delay(timeoutMs) {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(timeoutMs);
+        }, timeoutMs);
+    })
+}
+
+const promise1 = delay(1);
+const promise2 = delay(2);
+const promise3 = delay(3);
+
+Promise.all([promise1, promise2, promise3])
+.then((values) => {
+    console.log(values);
+});
+
+
+/**
+ * 上で定義したdummyFetch関数を使う
+ * resA, resBどちらを先に取得しても問題ない場合、Promise.allでまとめられる。
+ * 渡したPromiseが一つでもRejectになった場合は、エラーをcatchする。
+ */
+const fetchedPromise = Promise.all([
+    dummyFetch("/js/ajax.js"), dummyFetch("/js/index.js")
+]);
+
+fetchedPromise.then(([resA, resB]) => {
+    console.log(resA.body);
+    console.log(resB.body);
+}).catch ((err) => {
+    console.error(`エラー：${err}`);
+});
+
+/** 
+ * Promise.race
+ * Promise.raceは、全てのPromiseが完了するまで待つPromise.allと違い、複数のPromiseを受け取るが、一つでも完了したら次の処理を実行する。
+ * 
+ * 上で定義したdelay関数を使う
+ * 一つでもresolveまたはrejectした時点で次の処理を呼び出す
+ */ 
+const racePromise = Promise.race([
+    delay(1),    
+    delay(32),    
+    delay(64),    
+    delay(128),
+]);
+
+racePromise.then((value) => {
+    // 最も早く完了するのは1ミリ秒後
+    console.log(value);
+})
+
+/**
+ * 上で定義したdummyFetch関数を使う
+ * timeOutMs病後にタイムアウトする、timeOut関数を定義
+ * 500m秒以内に取得できなければ失敗時の処理が呼ばれる
+ */
+
+function timeOut(timeOutMs) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            reject(new Error(`timeOut：${timeOutMs}ミリ秒経過`));
+        }, timeOutMs);
+    })
+}
+
+Promise.race([
+    dummyFetch("/js/index.js"),
+    timeOut(600),
+]).then(res => {
+    console.log(res.body);
+}).catch(err => {
+    console.error(err.message);
+});
